6965414662238d0de21161c8102f5256258d624b
Jonathan <jonathanwnissen@gmail.com>
2019-05-28 10:31:15 +0200

    hellworld

---
+++ src/main/java/com/example/helloworldjenkins/HelloworldjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldjenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldjenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldjenkins/HelloworldjenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.helloworldjenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloworldjenkinsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
